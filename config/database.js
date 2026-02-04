e93933905390629e5c503a0716a6b99f7f757be2
Sanika M. Bhore <sanikambhore21@gmail.com>
2026-02-04 12:25:21 +0530

    t

--- config/database.js
+++ config/database.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: "ballast.proxy.rlwy.net",
+  user: "root",
+  password: "SZudvdfTeedMEzjrFTKEHjbFdcXwQdXk",
+  database: "railway",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
